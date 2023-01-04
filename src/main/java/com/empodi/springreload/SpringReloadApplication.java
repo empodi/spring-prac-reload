0db1f25166018a301425c0b3d6bcd1f692f4c707
empodi <kglein9513@gmail.com>
2023-01-04 17:19:13 +0900

    Initial commit

---
+++ src/main/java/com/empodi/springreload/SpringReloadApplication.java
@@ -0,0 +1,13 @@
+package com.empodi.springreload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReloadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empodi/springreload/SpringReloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empodi.springreload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
